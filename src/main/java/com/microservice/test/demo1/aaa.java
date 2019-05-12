387d3a1b2771b59f0d9e4d3b1b9bcfbe3b3614d2
ottomr <husnutapan1994@gmail.com>
2019-05-13 01:13:34 +0300

    test commit

---
+++ src/main/java/com/microservice/test/demo1/aaa.java
@@ -0,0 +1,76 @@
+package com.microservice.test.demo1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class aaa {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        if (head == null) {
+            head = new SinglyLinkedListNode(data);
+        } else {
+            SinglyLinkedListNode node = head;
+            while (node.next != null) {
+                node = node.next;
+            }
+            node.next = new SinglyLinkedListNode(data);
+        }
+        return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+
+            llist.head = llist_head;
+        }
+
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/com/microservice/test/demo1/controller/RestService.java
@@ -0,0 +1,14 @@
+package com.microservice.test.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestService {
+
+    @GetMapping("getInfo")
+    public String getInfo() {
+        return "service-1 called";
+    }
+
+}
